d8d04fa034c9300c25db710246bce12446d73bd8
julikroch <juliankrochmal@gmail.com>
2021-01-17 16:43:06 -0300

    creating movie searchform

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import 'bulma/css/bulma.css'
-import {Title} from './components/Title'
+import { Title } from './components/Title'
+import { SearchForm } from './components/SearchForm'
 
 function App() {
   return (
     <div className="App">
-     <Title>Search movies</Title>
+      <Title>Search movies</Title>
+      <div className="SearchForm-wrapper">
+        <SearchForm />
+      </div>
     </div>
   );
 }
